6a816a8c66b057756cb539c3998c518a9589b9ae
Yves BAYINGANA <yvantlx62@gmail.com>
2025-10-18 22:43:45 +0200

    Populating Your Database with Your Very First Data

---
+++ task_5.sql
@@ -0,0 +1,9 @@
+-- Use the database passed as an argument
+-- (You will execute this file like: mysql -u root -p alx_book_store < task_5.sql)
+
+USE alx_book_store;
+
+-- Insert a single record into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
