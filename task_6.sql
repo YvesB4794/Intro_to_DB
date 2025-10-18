74bc5658218d95b9aeabd95252dfa4bd249b41cd
Yves BAYINGANA <yvantlx62@gmail.com>
2025-10-18 22:47:13 +0200

    Populating Your Database with Your More Data

---
+++ task_6.sql
@@ -0,0 +1,12 @@
+-- Use the database passed as an argument
+-- (Run this file with: mysql -u root -p alx_book_store < task_6.sql)
+
+USE alx_book_store;
+
+-- Insert multiple rows into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES 
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+
