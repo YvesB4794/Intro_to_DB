09018fd18a26026d1ee7d306d164b8bcb38951b7
Yves BAYINGANA <yvantlx62@gmail.com>
2025-10-18 19:49:51 +0200

    Full description

---
+++ task_4.sql
@@ -0,0 +1,17 @@
+-- task_4.sql
+-- Script to print the full description of the table 'books' from a given database
+-- Do NOT use DESCRIBE or EXPLAIN
+
+SELECT 
+    COLUMN_NAME AS 'Column',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'books';
+
